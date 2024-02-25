2cc7ec5e288f49bd1c898d4e28c4e94cfd33f4c7
silvana02 <silvanalima.89@hotmail.com>
2024-02-26 00:10:56 +0100

    refactor footer and header styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import {
-  Flex,
+  Container,
   HStack,
   IconButton,
   Image,
@@ -17,72 +17,78 @@ import logoEmpresa from '../assets/logoEmpresa.svg';
 import logoMarretti from '../assets/logoMarretti.png';
 export const Footer = () => {
   return (
-    <Flex
-      backgroundColor="black"
-      color={'white'}
-      w={'100%'}
-      fontSize={['14px', '16px']}
-      p={6}
-      align={'center'}
-      justify={'space-around'}
-      direction={['column', 'row']}
-    >
-      <Stack as={Link} to={'/'} size={'sm'} pb={10}>
-        <Image
-          src={logoEmpresa}
-          alt="Logo Escaleras Goggi"
-          h={['80px', '100px']}
-        />
-      </Stack>
-      <VStack pb={10}>
-        <Text>Representantes de:</Text>
-        <Stack size={'sm'}>
-          <Image src={logoMarretti} alt="Logo Marreti" h={['30px', '40px']} />
+    <Stack w={'100%'} backgroundColor="black">
+      <Container
+        color={'white'}
+        maxW={'1200px'}
+        fontSize={['12px', '14px']}
+        pt={5}
+        pb={5}
+        display={'flex'}
+        justifyContent={'space-between'}
+        flexDirection={['column', 'column', 'row']}
+        gap={5}
+      >
+        <Stack as={Link} size={'sm'} to={'/'}>
+          <Image
+            src={logoEmpresa}
+            alt="Logo Escaleras Goggi"
+            h={['60px', '90px']}
+          />
         </Stack>
-      </VStack>
-      <VStack align={'center'} pb={8}>
-        <Text>Síguenos en nuestras redes sociales!!</Text>
-        <LinkChakra href="https://www.instagram.com/escalerasgoggi/" isExternal>
-          <IconButton
-            colorScheme="teal"
-            variant={'outline'}
-            aria-label="Link Instagram"
-            size="lg"
-            icon={<BsInstagram />}
-            borderRadius={'50%'}
-          />
-        </LinkChakra>
+        <VStack>
+          <Text>Representantes de:</Text>
+          <Stack size={'sm'}>
+            <Image src={logoMarretti} alt="Logo Marreti" h={['20px', '30px']} />
+          </Stack>
+        </VStack>
+        <VStack align={'center'}>
+          <Text>Síguenos en nuestras redes sociales!!</Text>
+          <LinkChakra
+            href="https://www.instagram.com/escalerasgoggi/"
+            isExternal
+          >
+            <IconButton
+              colorScheme="whiteAlpha"
+              variant={'outline'}
+              aria-label="Link Instagram"
+              size="lg"
+              icon={<BsInstagram />}
+              borderRadius={'50%'}
+            />
+          </LinkChakra>
 
-        <LinkChakra
-          href="https://www.facebook.com/profile.php?id=100069053231731"
-          isExternal
-        >
-          <IconButton
-            colorScheme="teal"
-            variant={'outline'}
-            aria-label="Link Facebook"
-            size="lg"
-            icon={<BsFacebook />}
-            borderRadius={'50%'}
-          />
-        </LinkChakra>
-      </VStack>
-      <VStack align={'initial'} pb={5}>
-        <Text fontWeight={'semibold'}>Contacto:</Text>
-        <HStack>
-          <AiOutlineMail /> <Text>info@escalerasmartinezlastra.es</Text>
-        </HStack>
-        <HStack>
-          <BsFillTelephoneFill /> <Text>942-26 20 85</Text>
-        </HStack>
-        <HStack>
-          <FaMapMarkerAlt />{' '}
-          <Text>
-            Polígono de Cros, Bloque 2 Naves 1 y 2 Maliaño. <br /> Camargo C.P
-            39600 Cantabria, España.
-          </Text>
-        </HStack>
-      </VStack>
-    </Flex>
+          <LinkChakra
+            href="https://www.facebook.com/profile.php?id=100069053231731"
+            isExternal
+          >
+            <IconButton
+              colorScheme="whiteAlpha"
+              variant={'outline'}
+              aria-label="Link Facebook"
+              size="lg"
+              icon={<BsFacebook />}
+              borderRadius={'50%'}
+            />
+          </LinkChakra>
+        </VStack>
+        <VStack align={['center', 'center', 'initial']}>
+          <Text fontWeight={'semibold'}>Contacto:</Text>
+          <HStack>
+            <AiOutlineMail /> <Text>info@escalerasmartinezlastra.es</Text>
+          </HStack>
+          <HStack>
+            <BsFillTelephoneFill /> <Text>942-26 20 85</Text>
+          </HStack>
+          <HStack>
+            <FaMapMarkerAlt />{' '}
+            <Text>
+              Polígono de Cros, Bloque 2 Naves 1 y 2 Maliaño. <br /> Camargo C.P
+              39600 Cantabria, España.
+            </Text>
+          </HStack>
+        </VStack>
+      </Container>
+    </Stack>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
-  Flex,
+  Container,
   IconButton,
   Image,
   Menu,
@@ -20,94 +20,97 @@ import logoEmpresa from '../assets/logoEmpresa.svg';
 
 export const Header = () => {
   return (
-    <Flex
-      as="header"
-      w={'100%'}
-      justify={'space-around'}
-      alignItems={'center'}
-      backgroundColor="rgba(0, 0, 0, 0.8)"
-      color={'white'}
-    >
-      <Stack as={Link} to={'/'} size={'sm'} m={3} mr={30}>
-        <Image src={logoEmpresa} alt="Logo" h={['60px', '90px']} />
-      </Stack>
+    <Stack backgroundColor="rgba(0, 0, 0, 0.8)" w={'100%'}>
+      <Container
+        as="header"
+        maxW={'1200px'}
+        color={'white'}
+        display={'flex'}
+        justifyContent={'space-between'}
+        alignItems={'center'}
+        p={3}
+      >
+        <Stack as={Link} to={'/'} size={'sm'}>
+          <Image src={logoEmpresa} alt="Logo" h={['60px', '90px']} />
+        </Stack>
 
-      <Show below="md">
-        <Menu>
-          <IconButton
-            as={MenuButton}
-            aria-label="Menu"
-            icon={<HamburgerIcon />}
-            variant={'outline'}
-          />
-          <Portal>
-            <MenuList>
-              <MenuList color={'black'}>
-                <MenuItem as={Link} to={'/'}>
-                  Inicio
-                </MenuItem>
-                <MenuItem as={Link} to={'/products'}>
-                  Productos
-                </MenuItem>
-                <MenuItem as={Link} to={'/empresa'}>
-                  La empresa
-                </MenuItem>
-                <MenuItem as={Link} to={'/contacto'}>
-                  Contacto
-                </MenuItem>
-              </MenuList>
-            </MenuList>
-          </Portal>
-        </Menu>
-      </Show>
-
-      <Show above="md">
-        <Breadcrumb mt={5} fontWeight="medium" fontSize="sm" separator="">
-          <BreadcrumbItem>
-            <BreadcrumbLink as={Link} to={'/'}>
-              INICIO
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-
+        <Show below="md">
           <Menu>
-            <MenuButton as={Link}>
-              PRODUCTOS
-              <ChevronDownIcon />
-            </MenuButton>
-            <MenuList color={'black'}>
-              <MenuItem as={Link} to={'/products'}>
-                Todos
-              </MenuItem>
-              <MenuItem as={Link} to={'/madera'}>
-                Madera
-              </MenuItem>
-              <MenuItem>Cristal</MenuItem>
-              <MenuItem>Acero inoxidable</MenuItem>
-              <MenuItem>Acabados Especiales</MenuItem>
-              <MenuItem>Barandillas</MenuItem>
-              <MenuItem>Rústicas</MenuItem>
-            </MenuList>
+            <IconButton
+              as={MenuButton}
+              aria-label="Menu"
+              icon={<HamburgerIcon />}
+              variant={'outline'}
+            />
+            <Portal>
+              <MenuList>
+                <MenuList color={'black'}>
+                  <MenuItem as={Link} to={'/'}>
+                    Inicio
+                  </MenuItem>
+                  <MenuItem as={Link} to={'/products'}>
+                    Productos
+                  </MenuItem>
+                  <MenuItem as={Link} to={'/empresa'}>
+                    La empresa
+                  </MenuItem>
+                  <MenuItem as={Link} to={'/contacto'}>
+                    Contacto
+                  </MenuItem>
+                </MenuList>
+              </MenuList>
+            </Portal>
           </Menu>
+        </Show>
 
-          <BreadcrumbItem ml={3}>
-            <BreadcrumbLink as={Link} to={'/empresa'}>
-              LA EMPRESA
-            </BreadcrumbLink>
-          </BreadcrumbItem>
+        <Show above="md">
+          <Breadcrumb mt={5} fontWeight="medium" fontSize="sm" separator="">
+            <BreadcrumbItem>
+              <BreadcrumbLink as={Link} to={'/'}>
+                INICIO
+              </BreadcrumbLink>
+            </BreadcrumbItem>
 
-          <BreadcrumbItem>
-            <BreadcrumbLink as={Link} to={'/novedades'}>
-              NOVEDADES
-            </BreadcrumbLink>
-          </BreadcrumbItem>
+            <Menu>
+              <MenuButton as={Link}>
+                PRODUCTOS
+                <ChevronDownIcon />
+              </MenuButton>
+              <MenuList color={'black'}>
+                <MenuItem as={Link} to={'/products'}>
+                  Todos
+                </MenuItem>
+                <MenuItem as={Link} to={'/madera'}>
+                  Madera
+                </MenuItem>
+                <MenuItem>Cristal</MenuItem>
+                <MenuItem>Acero inoxidable</MenuItem>
+                <MenuItem>Acabados Especiales</MenuItem>
+                <MenuItem>Barandillas</MenuItem>
+                <MenuItem>Rústicas</MenuItem>
+              </MenuList>
+            </Menu>
 
-          <BreadcrumbItem>
-            <BreadcrumbLink as={Link} to={'/contacto'}>
-              CONTACTO
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-        </Breadcrumb>
-      </Show>
-    </Flex>
+            <BreadcrumbItem ml={3}>
+              <BreadcrumbLink as={Link} to={'/empresa'}>
+                LA EMPRESA
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbItem>
+              <BreadcrumbLink as={Link} to={'/novedades'}>
+                NOVEDADES
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbItem>
+              <BreadcrumbLink as={Link} to={'/contacto'}>
+                CONTACTO
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>
+        </Show>
+      </Container>
+    </Stack>
   );
 };
